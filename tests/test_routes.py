c0ce6ccb3a3b0cbdbddf5d98a60d648caae8ca8c
Dmitry Karlov <dkar89@gmail.om>
2026-04-11 14:10:35 -0400

    Delete method and the test cases have been implemented

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -165,3 +165,11 @@ class TestAccountService(TestCase):
         self.assertEqual(new_response.status_code, status.HTTP_200_OK)
         updated_account = new_response.get_json()
         self.assertEqual(updated_account["name"], "TestName")
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(
+            f"{BASE_URL}/{account.id}"
+            )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
